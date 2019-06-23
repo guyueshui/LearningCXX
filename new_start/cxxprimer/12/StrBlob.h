b7a8d091f6ff628e0b2fb5eecf156c1ba964fa85
guyueshui <guyueshui002@gmail.com>
2019-06-24 00:30:18 +0800

    - Add `StrBlobPtr` in `StrBlob.h` - Add some interface to `StrBlob`

--- new_start/cxxprimer/12/StrBlob.h
+++ new_start/cxxprimer/12/StrBlob.h
@@ -5,24 +5,62 @@
 #include <string>
 #include <vector>
 
+class StrBlob; // declaration
+class StrBlobPtr // a class for manage pointers to StrBlob
+{
+public:
+    // ctors
+    StrBlobPtr(): curr(0) { }
+    StrBlobPtr(StrBlob&, size_t sz = 0);
+
+    // APIs
+    std::string& deref() const;
+    StrBlobPtr& incr(); // prefix ++
+
+    // operator overload
+    std::string& operator*();
+    const std::string& operator*() const;
+    StrBlobPtr& operator++(); // prefix++
+    bool operator!=(const StrBlobPtr& rhs) const
+    {
+        return rhs.curr != curr;
+    }
+    
+private:
+    std::weak_ptr<std::vector<std::string>> wptr;
+    size_t curr;
+    std::shared_ptr<std::vector<std::string>>
+        check(size_t, const std::string&) const;
+}; // StroBlobPtr
+
 class StrBlob {
+    friend StrBlobPtr::StrBlobPtr(StrBlob&, size_t);
 public:
     typedef std::vector<std::string>::size_type size_type;
+    // ctors
     StrBlob();
     StrBlob(std::initializer_list<std::string> il);
+
+    // APIs
     size_type size() const { return data -> size(); }
     bool empty() const { return data -> empty(); }
+
     // modifiers
     void push_back(const std::string& t) {
         data -> push_back(t);
     }
     void pop_back();
+
     // selectors
+    StrBlobPtr begin() const;
+    StrBlobPtr end() const;
     std::string& front();
     std::string& back();
+    const std::string& front() const;
+    const std::string& back() const;
 private:
     std::shared_ptr<std::vector<std::string>> data;
     void check(size_type i, const std::string& msg) const;
-};
+}; // StrBlob
 
 #endif // STRBLOB_H
--- new_start/cxxprimer/12/StrBlob.hpp
+++ new_start/cxxprimer/12/StrBlob.hpp
@@ -14,13 +14,36 @@ void StrBlob::check(size_type i, const std::string& msg) const
 }
 
 // selectors
-std::string& StrBlob::front() {
+StrBlobPtr StrBlob::begin() const
+{
+    check(0, "begin on empty StrBlob");
+    return StrBlobPtr(const_cast<StrBlob&>(*this), 0);
+}
+StrBlobPtr StrBlob::end() const
+{
+    check(0, "end on empty StrBlob");
+    return StrBlobPtr(const_cast<StrBlob&>(*this), 0 + this->size());
+}
+const std::string& StrBlob::front() const {
     check(0, "front on empty StrBlob");
     return data -> front();
 }
-std::string& StrBlob::back() {
+const std::string& StrBlob::back() const {
     check(0, "back on empty StrBlob");
     return data -> back();
+}
+// avoid duplications using const_cast
+std::string& StrBlob::front()
+{
+    return const_cast<std::string&>(
+            static_cast<const StrBlob&>(*this).front()
+            );
+}
+std::string& StrBlob::back()
+{
+    return const_cast<std::string&>(
+            static_cast<const StrBlob&>(*this).back()
+            );
 }
 void StrBlob::pop_back() {
     check(0, "pop_back on empty StrBlob");
