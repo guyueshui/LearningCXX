a8abe54f80f55f17263c3ea5afb6123d418436dc
guyueshui <guyueshui002@gmail.com>
2019-10-16 11:30:28 +0800

    Review for insert_sort

--- sort_algos/insert_sort.cpp
+++ sort_algos/insert_sort.cpp
@@ -15,10 +15,14 @@ using std::endl;
 // sorting elems in at range [beg, end) into ascending order.
 void insert_sort(vector<int>& arr, size_t beg, size_t end) {
     if (end - beg > 1) {
-        for (auto to_sort = beg + 1; to_sort != end; ++to_sort) {
+        for (int to_sort = beg + 1; to_sort < (int)end; ++to_sort) {
             // the first elem is assumed to be sorted
-            auto x = to_sort;
-            for (auto j = to_sort - 1; j >= beg; --j) {
+/**
+ * Review: this may cause overflow, if j = 0, j-1 will be very large
+ * if j is size_t.
+ */
+            int x = to_sort;
+            for (int j = to_sort - 1; j >= (int)beg; --j) {
                 if (arr[x] < arr[j]) {
                     std::swap(arr[x], arr[j]);
                     --x;
